7a628a7a89d7ddbef7db9e6f7d730d501a7e4c74
José Alvarez C. <alvarez.jo.developer@gmail.com>
2025-08-01 00:51:54 +0800

    Fix layout props bug, make layout async

--- next-app/src/app/(bible)/page.tsx
+++ next-app/src/app/(bible)/page.tsx
@@ -1,14 +1,12 @@
-// import ChapterPage from "./[book]/[chapter]/page";
+import ChapterPage from "./[book]/[chapter]/page";
 
 export default function Home() {
   // call your same ChapterPage component
 
   // build the same shape Next.js normally passes
-  //   const defaultParams = Promise.resolve({
-  //     book: "Genesis",
-  //     chapter: "1",
-  //   });
-  //   return <ChapterPage params={defaultParams} />;
-
-  return <>HELLO</>;
+  const defaultParams = Promise.resolve({
+    book: "Genesis",
+    chapter: "1",
+  });
+  return <ChapterPage params={defaultParams} />;
 }
